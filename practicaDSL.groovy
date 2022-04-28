d3f70692a6d68abf77e786151b29d9bf8b3b22b0
Victor Vizcarra <54164831+victorvizcarra@users.noreply.github.com>
2022-04-27 18:31:38 -0700

    Create practicaDSL.groovy

---
+++ practicaDSL.groovy
@@ -0,0 +1,44 @@
+job('Practica Node.js Docker DSL') {
+    description('Practica de publicación en Docker-hub')
+    scm {
+        git('https://github.com/victorvizcarra/PracticasDesarrollo.git', 'main') { node ->
+            node / gitConfigName('victorvizcarra')
+            node / gitConfigEmail('victorvizcarra@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('victorvizcarra/PracticasDesarrollo')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
